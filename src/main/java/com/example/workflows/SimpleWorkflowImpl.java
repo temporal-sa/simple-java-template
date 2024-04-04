9e0985180cb808bb9ec71d0add407233c59866af
Peter Sullivan <pvsone@gmail.com>
2024-04-04 00:17:38 -0500

    add back the workflow logger

--- src/main/java/com/example/workflows/SimpleWorkflowImpl.java
+++ src/main/java/com/example/workflows/SimpleWorkflowImpl.java
@@ -11,15 +11,17 @@ import io.temporal.spring.boot.WorkflowImpl;
 import io.temporal.workflow.Async;
 import io.temporal.workflow.Promise;
 import io.temporal.workflow.Workflow;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 
 import java.time.Duration;
 
-@Slf4j
+
 @WorkflowImpl(workers = "simple-worker")
 public class SimpleWorkflowImpl implements SimpleWorkflow {
 
-    ActivityOptions activityOptions = ActivityOptions.newBuilder()
+    private static final Logger log = Workflow.getLogger(SimpleWorkflowImpl.class);
+    
+    private final ActivityOptions activityOptions = ActivityOptions.newBuilder()
             .setStartToCloseTimeout(Duration.ofSeconds(5))
             .setRetryOptions(RetryOptions.newBuilder()
                     .setInitialInterval(Duration.ofSeconds(1))
